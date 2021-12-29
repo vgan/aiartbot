09adbb9aea092d5efb094283628aa0838ac6e2f8
Steve Cvar <github@stevecvar.com>
2021-12-28 19:57:26 -0600

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM vault.habana.ai/gaudi-docker/1.1.0/ubuntu18.04/habanalabs/tensorflow-installer-tf-cpu-2.6.0:1.1.0-614
+RUN apt update
+RUN apt install -y git vim
+RUN git clone https://github.com/openai/CLIP
+RUN git clone https://github.com/CompVis/taming-transformers
+RUN git clone https://github.com/vgan/AWS_Deep_Learning_Challenge_2022.git
+RUN pip install torch==1.9.0+cpu torchvision==0.10.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+RUN pip install ftfy regex tqdm omegaconf pytorch-lightning tweepy Mastodon.py imageio pandas seaborn kornia einops==0.3.0 transformers==4.3.1 google-api-python-client google-auth-httplib2 google-auth-oauthlib
+RUN chmod u+x AWS_Deep_Learning_Challenge_2022/aiartbot.py
+RUN mkdir -p "/root/.cache/torch/hub/checkpoints"
+RUN curl "https://download.pytorch.org/models/vgg16-397923af.pth" -o "/root/.cache/torch/hub/checkpoints/vgg16-397923af.pth"
+RUN ln -s /mnt/AWS_Deep_Learning_Challenge_2022/models /AWS_Deep_Learning_Challenge_2022/
+RUN ln -s /mnt/AWS_Deep_Learning_Challenge_2022/outputs /AWS_Deep_Learning_Challenge_2022/
+WORKDIR "/AWS_Deep_Learning_Challenge_2022/"
+ADD VERSION .
---
+++ extra.py
@@ -0,0 +1,75 @@
+# Utils
+import os
+import sys
+import math
+import warnings
+from pathlib import Path
+from tqdm import tqdm 
+from base64 import b64encode
+import imageio
+import datetime
+__utils__ = [
+    "os", "sys", "Path", "tqdm",
+    "datetime", "imageio", "b64encode", "warnings", "math"
+]
+
+# DataScience-CPU
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+__cpu_all__ = [
+    "plt", "sns", "np", "pd", "Image"
+]
+
+# DataScience-GPU
+try:
+    import cupy as cp
+    import cudf
+    import cuml
+    __gpu_all__ = ["cp", "cudf", "cuml"]
+except:
+    __gpu_all__ = []
+    #warnings.warn("GPU skipped!")
+
+# Distributed DataScience
+try:
+    import dask_cudf
+    __dist_all__ = ["dask_cudf"]
+except:
+    __dist_all__ = []
+    #warnings.warn("Dask skipped!")
+
+# VQGAN+CLIP
+try:
+    from omegaconf import OmegaConf
+    sys.path.append('..')
+    sys.path.append('../taming-transformers')
+    from taming.models import cond_transformer, vqgan
+    import torch
+    from torch import nn, optim
+    from torch.nn import functional as F
+    from torchvision import transforms
+    from torchvision.transforms import functional as TF
+
+    from CLIP import clip
+    import kornia.augmentation as K
+    __vqgan_clip__ = [
+        "OmegaConf", "clip", "K", "TF", "F", "transforms",
+        "nn", "optim", "torch", "cond_transformer", "vqgan",
+    ]
+except:
+    __vqgan_clip__ = []
+    raise OSError("Can't import VQGAN+CLIP requirements.")
+
+def reset_kernel(): 
+    os._exit(00)
+
+__all__ = [
+    "reset_kernel" 
+]
+__all__ += __utils__
+__all__ += __cpu_all__
+__all__ += __dist_all__
+__all__ += __vqgan_clip__
